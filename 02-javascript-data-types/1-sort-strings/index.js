fc239ad306b999fb294a4519a7ed145900dfd35c
kit <mr.marashli@gmail.com>
2020-10-05 12:18:32 +0300

    add sort string function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,24 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  if (param === 'asc') {
+    return [...arr].sort(ascSort);
+  }
+  else if (param === 'desc') {
+    return [...arr].sort(ascSort).reverse();
+  }
 
+}
+
+function ascSort(a, b) {
+  const compareResult = a.localeCompare(b, 'default', {caseFirst: "upper"});
+  if (compareResult === 1) {
+    return 1;
+  }
+  if (compareResult === -1) {
+    return -1;
+  } 
+  if (compareResult === 0) {
+    return 0;
+  }
 }
